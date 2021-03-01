4e77ed5b31b846ad2fb7e4e1079a83e4070478ab
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-03-01 12:19:10 +0300

    TimelineManager Spawn tracks the state of the timelines correctly.

--- timeline_manager_test.go
+++ timeline_manager_test.go
@@ -1,18 +1,50 @@
-package timeline_test
+package timeline
 
 import (
     "testing"
-
-    "github.com/kuredoro/timeline"
+    "reflect"
 )
 
-func TestHello(t *testing.T) {
-    t.Run("hello prints hello", func(t *testing.T) {
-        got := timeline.Hello()
-        want := "Hello"
+func TestTimelineManagerSpawn(t *testing.T) {
+    t.Run("spawn when no timelines present", func(t *testing.T) {
+        manager := &TimelineManager{}
 
-        if got != want {
-            t.Errorf("hello returned %q, want %q", got, want)
+        manager.Spawn()
+
+        want := []int{1}
+        if !reflect.DeepEqual(manager.timelines, want) {
+            t.Errorf("got timelines %v, want %v", manager.timelines, want)
+        }
+    })
+
+    t.Run("spawn two timelines", func(t *testing.T) {
+        manager := &TimelineManager{}
+
+        manager.Spawn()
+        manager.Spawn()
+
+        want := []int{1, 2}
+        if !reflect.DeepEqual(manager.timelines, want) {
+            t.Errorf("got timelines %v, want %v", manager.timelines, want)
+        }
+    })
+
+    t.Run("spawn one when there's a hole between timelines", func(t *testing.T) {
+        manager := &TimelineManager{
+            timelines: []int{1, 2, 0, 4},
+            lastTimelineID: 4,
+        }
+
+        manager.Spawn()
+
+        want := []int{1, 2, 5, 4}
+        if !reflect.DeepEqual(manager.timelines, want) {
+            t.Errorf("got timelines %v, want %v", manager.timelines, want)
         }
     })
 }
+
+/*
+func TestTimelineManagerColumnGaps(t *testing.T) {
+}
+*/
