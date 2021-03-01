8696e38acea120422411a59479ff91c78193f366
しずえ <cauchymoon@mail.ru>
2021-03-01 12:19:10 +0300

    TimelineManager Spawn tracks the state of the timelines correctly.

--- timeline_manager.go
+++ timeline_manager.go
@@ -1,5 +1,33 @@
 package timeline
 
-func Hello() string {
-    return "Hello"
+// TimeManager will have sinks of type FormattedSink
+// It will have a func print(header, message)
+// It will be able to text wrap message how ever it likes
+// The sinks will also have Colorizers that given the column and the id of the timeline
+// will output corresponding color for the bar...
+// I.e., transform the symbol for bar...
+//
+
+type TimelineManager struct {
+    timelines []int
+    lastTimelineID int
+}
+
+func (tm *TimelineManager) Spawn() bool {
+    firstZero := 0
+    for ; firstZero < len(tm.timelines); firstZero++ {
+        if tm.timelines[firstZero] == 0 {
+            break
+        }
+    }
+
+    tm.lastTimelineID++
+
+    if firstZero != len(tm.timelines) {
+        tm.timelines[firstZero] = tm.lastTimelineID
+    } else {
+        tm.timelines = append(tm.timelines, tm.lastTimelineID)
+    }
+
+    return false
 }
