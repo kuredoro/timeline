dff987477f87a02053fa0f097e908641a260c7f6
しずえ <cauchymoon@mail.ru>
2021-03-01 23:16:28 +0300

    Refactor: put ticks and postfixes inside a map in the TimelineStyle and use them inside Timeline.

--- timeline.go
+++ timeline.go
@@ -5,10 +5,10 @@ import "fmt"
 type TimelineState int
 
 const (
-    c_start TimelineState = iota
-    c_inprogress
-    c_lastwords
-    c_finished
+    START TimelineState = iota
+    INPROGRESS
+    LASTWORDS
+    FINISHED
 )
 
 type Timeline struct {
@@ -19,23 +19,28 @@ type Timeline struct {
 }
 
 func (t *Timeline) Println(args ...interface{}) {
+    if t.state == FINISHED {
+        return
+    }
+
+    tick := t.style.Tick[t.state]
+    postfix := t.style.Postfix[t.state]
+
     msg := fmt.Sprintln(args...)
 
-    if t.state == c_start {
-        t.manager.print([]string{t.style.StartTick, t.style.Dash, t.style.Space}, msg)
-        t.state = c_inprogress
-    } else if t.state == c_inprogress {
-        t.manager.print([]string{t.style.InterTick, t.style.Dash, t.style.Space}, msg)
-    } else if t.state == c_lastwords {
-        t.manager.print([]string{t.style.FinalTick, t.style.Dash, t.style.Space}, msg)
-        t.state = c_finished
+    t.manager.print([]string{tick}, postfix + msg)
+
+    if t.state == START {
+        t.state = INPROGRESS
+    } else if t.state == LASTWORDS {
+        t.state = FINISHED
     }
 }
 
 func (t *Timeline) End() {
-    if t.state == c_finished {
+    if t.state == FINISHED {
         return
     }
 
-    t.state = c_lastwords
+    t.state = LASTWORDS
 }
